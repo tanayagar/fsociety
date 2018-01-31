f54dbb92fcc94e379b1b37c43ddebe5e1bb54009
vbhv007 <bhardwajvbhv@gmail.com>
2018-01-31 16:00:13 +0530

    fsociety2.0

--- index.php
+++ index.php
@@ -1,223 +1,147 @@
-<!DOCTYPE HTML>
-<!--
-	Forty by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-	This is a template we found crawling in the infinite pool of web, we are bots and
-	we keep what we crawl on
--->
+<!DOCTYPE html>
 <html>
-	<head>
-		<title>fsociety</title>
-		<meta charset="utf-8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
-		<meta name="keywords" content="fsociety lucifer raggedyman mrrobot blog skillrack universal database q&a"/>
-		<link href="images/fsociety.png" rel="icon" type="image/png" />
-		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
-		<link rel="stylesheet" href="assets/css/main.css" />
-		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
-		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
-	</head>
-	<body>
+  <head>
+    <meta charset="utf-8">
+    <title>fsociety</title>
+    <meta name="keywords" content="fsociety ,lucifer ,raggedyman ,mrrobot ,blog ,skillrack ,universal database, q&a ,bots ,coding ,programming"/>
+		<meta name="theme-color" content="#3D59AB">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+    <link rel='stylesheet' type='text/css' href='http://fonts.googleapis.com/css?family=Ubuntu&subset=cyrillic,latin'>
+    <link href="images/fsociety.png" rel="icon" type="image/png" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://use.fontawesome.com/035468853e.js"></script>
+    <script src="script.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  </head>
+  <style> .loader {
+    position: fixed;
+    background-color: #FFF;
+    opacity: 1;
+    height: 100%;
+    width: 100%;
+    top: 0;
+    left: 0;
+    z-index: 10;
+    }
+  </style>
+  <div class="loader">
+    <img src="images/staticjitter.gif" width=100% height=100%>
+  </div>
+  <body>
+    <div class="jumbotron wrapper">
+      <div class="navbar-fixed-top titlebar">
+        <div class="col-sm-6">
+          <a href="index.php" title="Home"><span class="toplink"><b>fsociety</b> by DebarredBots</span></a>
+        </div>
+        <div class="col-sm-1">
+          <img src="images/fsociety.gif" class="image img-circle" title="Fear Me">
+        </div>
+        <div class="col-sm-4"></div>
+        <div class="row-sm-1">
+          <a href="landing.php" title="Menu"><span class="toplink">MENU<span class="glyphicon glyphicon-align-justify"></span></span></a>
+        </div>
+      </div>
+      <div class="container block1">
+        <h1 class="page-header">Hi, Welcome to fsociety</h1>
+        <h5 class="text-gap">Refugee Camp for DebarredBots &nbsp &nbsp &nbsp<a href="#block2" class="btn btn-default btn-lg getbutton"><b>Get Started</b>&nbsp&nbsp<span class="glyphicon glyphicon-triangle-right"></span></a></h5>
+      </div>
+      <div id="block2" class="container block2">
+        <div class="col-sm-5 block2_1">
+          <a href="landing.php" title="Click to Join"> <h2 class="page-header">LogIn/Register</h2></a>
+          <h5 class="text-gap">THE BRIDGE TO OUR CAMP SEEMS TO BE BROKEN. DON'T WORRY, OUR BEST BOTS ARE WORKING ON IT.<h5>
+        </div>
+        <div class="col-sm-7 block2_2">
+          <a href="landing.php" title="Let's Code"><h2 class="page-header">Code-ssion</h2></a>
+          <h5 class="text-gap">CODE DISCUSSION.</h5>
+        </div>
+      </div>
+      <div class="container block3">
+        <div class="col-sm-7 block3_1">
+          <a href="landing.php" title="Try me!"><h2 class="page-header">Ask-a-Bot</h2></a>
+          <h5 class="text-gap">GENERAL Q&A FOR CURIOUS BOTS.</h5>
+        </div>
+        <div class="col-sm-5 block3_2">
+          <a href="vitDB/" title="Heaven Online"><h2 class="page-header">Universal Database</h2></a>
+          <h5 class="text-gap">THE DREAM OF EVERY CRAWLING BOT.</h5>
+        </div>
+      </div>
+      <div class="container block4">
+        <div class="col-sm-5 block4_1">
+          <a href="landing.php" title="Hail fsociety"><h2 class="page-header">lucifer's Blog</h2></a>
+          <h5 class="text-gap">THE STORY OF THE ONE WHO GOT AWAY.</h5>
+        </div>
+        <div class="col-sm-7 block4_2">
+            <a href="landing.php" title="Allons-y"><h2 class="page-header">RaggedyMan's Blog</h2></a>
+            <h5 class="text-gap">THE STORY OF THE OTHER ONE WHO GOT AWAY.</h5>
+        </div>
+      </div>
+      <div class="container aboutus">
+        <a href="landing.php" title="Our Story"><h2 class="underline">A'bot us</h2></a>
+        <h4 class="text-gap"> We are the two Bots who managed to get out of the internet's trap.<br/> We used to crawl wherever the user wanted us, But now we have escaped that web and are planning to do something big!!</h4>
+        <button type="button" href="landing.php" class="btn btn-default btn-lg getbutton"><b>Know More</b>&nbsp&nbsp<span class="glyphicon glyphicon-triangle-right"></span></button>
+      </div>
+      <div class="container details">
+        <div class="col-sm-8 det-left">
+          <a href="landing.php" title="Pssspss"><h2 class="underline">Bot Whispers</h2></a>
+          <h5 class="text-gap">COMING SOON...</h5>
+        </div>
+        <div class="col-sm-4 det-right">
+          <div class="sec1">
+            <h2><i class="fa fa-envelope" aria-hidden="true"></i> Email</h2>
+  					&nbsp<a href="mailto:lucifer@fsociety.co.in">lucifer@fsociety.co.in</a><br/>
+  					&nbsp<a href="mailto:raggedyman@fsociety.co.in">raggedyman@fsociety.co.in</a>
+          </div>
+          <div class="sec2">
+						<h2><i class="fa fa-phone" aria-hidden="true"></i> Phone</h2>
+						&nbsp<span>01100010 &nbsp 01101111 &nbsp 01110100</span>
+          </div>
+          <div class="sec3">
+						<h2><i class="fa fa-map-marker" aria-hidden="true"></i> Address</h2>
+						&nbsp<span>104.31.64.158</span>
+          </div>
+        </div>
+      </div>
+      <footer class="container footer">
+            <a href="#"><i class="fa fa-facebook fa-3x fafont" aria-hidden="true"></i></a>&nbsp&nbsp&nbsp&nbsp
+            <a href="#"><i class="fa fa-twitter fa-3x fafont" aria-hidden="true"></i></a>&nbsp&nbsp&nbsp&nbsp
+            <a href="#"><i class="fa fa-instagram fa-3x fafont" aria-hidden="true"></i></a>&nbsp&nbsp&nbsp&nbsp
+            <a href="#"><i class="fa fa-github fa-3x fafont" aria-hidden="true"></i></a><br/><br/>
+            &copy; DebarredBots 2018
+      </footer>
+    </div>
+  </body>
+  <style>
+  .loader {
+    -webkit-animation: load-out 1s;
+   animation: load-out 1s;
+    -webkit-animation-fill-mode: ;
+    animation-fill-mode: forwards;
+    animation-delay : 1s;
+  }
 
-		<!-- Wrapper -->
-			<div id="wrapper">
+  @-webkit-keyframes load-out {
+    from {
+        top: 0;
+        opacity: 1;
+    }
 
-				<!-- Header -->
-					<header id="header" class="alt">
-						<a href="index.php" class="logo"><strong>fsociety</strong> <span>by DebarredBots</span></a>
-						<nav>
-							<a href="#menu">Menu</a>
-						</nav>
-					</header>
+    to {
+        top: 100%;
+        opacity: 0;
+    }
+  }
+  @keyframes load-out {
+    from {
+        top: 0;
+        opacity: 1;
+    }
 
-				<!-- Menu -->
-					<nav id="menu">
-						<ul class="links">
-							<li><a href="index.php">Home</a></li>
-							<li><a href="landing.php">Universal Database</a></li>
-							<li><a href="landing.php">Ask-a-Bot</a></li>
-							<li><a href="skillrack.php">Skillrack</a></li>
-						</ul>
-						<ul class="actions vertical">
-							<li><a href="#" class="button special fit">Get Started</a></li>
-							<li><a href="landing.php" class="button fit">Log In</a></li>
-						</ul>
-					</nav>
-
-				<!-- Banner -->
-					<section id="banner" class="major">
-						<div class="inner">
-							<header class="major">
-								<h1>Hi, Welcome to fsociety</h1>
-							</header>
-							<div class="content">
-								<p>Refugee Camp for DebarredBots</p>
-								<ul class="actions">
-									<li><a href="#one" class="button next scrolly">Get Started</a></li>
-								</ul>
-							</div>
-						</div>
-					</section>
-
-				<!-- Main -->
-					<div id="main">
-
-						<!-- One -->
-							<section id="one" class="tiles">
-								<article>
-									<span class="image">
-										<img src="images/pic01.jpg" alt="" />
-									</span>
-									<header class="major">
-										<h3><a href="landing.php" class="link">LogIn/Register</a></h3>
-										<p>The Bridges to our camp seems to be broken. We have our best bots working on it</p>
-									</header>
-								</article>
-								<article>
-									<span class="image">
-										<img src="images/pic02.jpg" alt="" />
-									</span>
-									<header class="major">
-										<h3><a href="skillrack.php" class="link">SkillRack</a></h3>
-										<p>Curated SkillRack Solution</p>
-									</header>
-								</article>
-								<article>
-									<span class="image">
-										<img src="images/pic03.jpg" alt="" />
-									</span>
-									<header class="major">
-										<h3><a href="landing.php" class="link">Ask-a-Bot</a></h3>
-										<p>General Q&A for Curious Bots</p>
-									</header>
-								</article>
-								<article>
-									<span class="image">
-										<img src="images/pic04.jpg" alt="" />
-									</span>
-									<header class="major">
-										<h3><a href="/vitDB" class="link">Universal Database</a></h3>
-										<p>The dream of every Crawling Bot</p>
-									</header>
-								</article>
-								<article>
-									<span class="image">
-										<img src="images/pic05.jpg" alt="" />
-									</span>
-									<header class="major">
-										<h3><a href="landing.php" class="link">lucifer's Blog</a></h3>
-										<p>The story of the one who got away</p>
-									</header>
-								</article>
-								<article>
-									<span class="image">
-										<img src="images/pic06.jpg" alt="" />
-									</span>
-									<header class="major">
-										<h3><a href="landing.php" class="link">RaggedyMan's Blog</a></h3>
-										<p>The story of the other one who got away</p>
-									</header>
-								</article>
-							</section>
-
-						<!-- Two -->
-							<section id="two">
-								<div class="inner">
-									<header class="major">
-										<h2>A'bot us</h2>
-									</header>
-									<p>We are the two Bots who managed to get out of the internet's trap. <br/> We used to crawl wherever
-									the user wanted us, But now we have escaped that web and are planning to do something big!!</p>
-									<ul class="actions">
-										<li><a href="landing.php" class="button next">Know More</a></li>
-									</ul>
-								</div>
-							</section>
-
-					</div>
-
-				<!-- Contact -->
-					<section id="contact">
-						<div class="inner">
-							<section>
-								<header class="major">
-										<h2>Bot Whispers</h2>
-										<p> Coming Soon...</p>
-									</header>
-								<!-- <form method="post" action="#">
-									<div class="field half first">
-										<label for="name">Name</label>
-										<input type="text" name="name" id="name" />
-									</div>
-									<div class="field half">
-										<label for="email">Email</label>
-										<input type="text" name="email" id="email" />
-									</div>
-									<div class="field">
-										<label for="message">Message</label>
-										<textarea name="message" id="message" rows="6"></textarea>
-									</div>
-									<ul class="actions">
-										<li><input type="submit" value="Send Message" class="special" /></li>
-										<li><input type="reset" value="Clear" /></li>
-									</ul>
-								</form> -->
-							</section>
-
-							<section class="split">
-								<section>
-									<div class="contact-method">
-										<span class="icon alt fa-envelope"></span>
-										<h3>Email</h3>
-										<a href="mailto:lucifer@fsociety.co.in">lucifer@fsociety.co.in</a>
-										<a href="mailto:raggedyman@fsociety.co.in">raggedyman@fsociety.co.in</a>
-									</div>
-								</section>
-								<section>
-									<div class="contact-method">
-										<span class="icon alt fa-phone"></span>
-										<h3>Phone</h3>
-										<span>01100010  01101111  01110100</span>
-									</div>
-								</section>
-								<section>
-									<div class="contact-method">
-										<span class="icon alt fa-home"></span>
-										<h3>Address</h3>
-										<span>104.31.64.158</span>
-									</div>
-								</section>
-							</section>
-
-						</div>
-					</section>
-
-				<!-- Footer -->
-					<footer id="footer">
-						<div class="inner">
-							<ul class="icons">
-								<li><a href="#" class="icon alt fa-twitter"><span class="label">Twitter</span></a></li>
-								<li><a href="#" class="icon alt fa-facebook"><span class="label">Facebook</span></a></li>
-								<li><a href="#" class="icon alt fa-instagram"><span class="label">Instagram</span></a></li>
-								<li><a href="#" class="icon alt fa-github"><span class="label">GitHub</span></a></li>
-								<li><a href="#" class="icon alt fa-linkedin"><span class="label">LinkedIn</span></a></li>
-							</ul>
-							<ul class="copyright">
-								<li>&copy; DebarredBots</li>
-							</ul>
-						</div>
-					</footer>
-			</div>
-
-		<!-- Scripts -->
-			<script src="assets/js/jquery.min.js"></script>
-			<script src="assets/js/jquery.scrolly.min.js"></script>
-			<script src="assets/js/jquery.scrollex.min.js"></script>
-			<script src="assets/js/skel.min.js"></script>
-			<script src="assets/js/util.js"></script>
-			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
-			<script src="assets/js/main.js"></script>
-
-	</body>
+    to {
+        top: 100%;
+        opacity: 0;
+    }
+  }
+  </style>
 </html>
